7885f15d84fb9eafb400b429a4bf133b126158b2
CaoKy.Nguyen <caoky.nguyen.1704@gmail.com>
2023-05-06 10:29:25 +0700

    up

---
+++ Thời gian thực/TimeReal.lua
@@ -0,0 +1,18 @@
+--[=[
+    DevMini Viet Nam
+    Link: https://www.facebook.com/groups/313279116516482
+    Arthur: Kỳ Kỳ
+    Ý tưởng thuộc về Kỳ Kỳ
+    Code không được mã hoá nhầm mục đích học tập
+]=]--
+function GameUpdate05062023()
+    local t = os.time()
+    local dateTable = os.date("*t", t)
+    VarLib2:setGlobalVarByName(3,"gio",dateTable.hour)
+    VarLib2:setGlobalVarByName(3,"phut",dateTable.min)
+    VarLib2:setGlobalVarByName(3,"giay",dateTable.sec)
+    VarLib2:setGlobalVarByName(3,"ngay",dateTable.day)
+    VarLib2:setGlobalVarByName(3,"thang",dateTable.month)
+    VarLib2:setGlobalVarByName(3,"nam",dateTable.year)
+end
+ScriptSupportEvent:registerEvent([=[Game.RunTime]=], GameUpdate05062023)
